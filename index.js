1f0d5fa4d6d6cc7dc74f0f1c17ec045de6a59fa0
2Epik4u <justinabossmlg@gmail.com>
2020-07-13 17:11:11 -0400

    added tingz

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '@2Epik4u') {
+    msg.reply('im sad bc you ping 2epik4u pls dont ping him again https://tenor.com/view/cute-cat-cry-three-sad-gif-15849691')
+  } else if (msg.content === 'importing' ) {
+    msg.reply('never')
+} else if (msg.content === 'sign up' ) {
+    msg.reply('when i learn php')
+ } else if (msg.content === 'hi' ) {
+    msg.reply('hi how are u doing')
+  } else if (msg.content === 'patched' ) {
+     msg.reply('Wrapper Online isnt patched.')
+   } else if (msg.content === 'your grounded' ) {
+      msg.reply('no u')
+    } else if (msg.content === 'i hate raiders' ) {
+       msg.reply('me too they suck they ruin all the fun')
+     
+
+
+
+}
+});
+
+client.login('NjI4NjA3MTkyODA2OTgxNjMy.XZNyCw.cNjUuecSjTtPdMUK76nm1bVLqBc');
