34080da1d1a1690f2b399bb7e187b34a8f34e5f0
2Epik4u <justinabossmlg@gmail.com>
2020-07-14 09:42:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ client.on('message', msg => {
  if (msg.content.includes('importing')) {
   msg.reply('never lol just edit it in dummy');
      
-} else if (msg.content.includes('<@628607192806981632>')) {
-  msg.reply('Did you just fricken ping me. Thats it! you are grounded grounded grounded FOREVER.');
+
     
 
 
