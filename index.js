a3ea479c14be7742072b574db753abefe6e472e5
2Epik4u <justinabossmlg@gmail.com>
2020-07-13 20:44:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,19 +13,22 @@ client.on('message', msg => {
 client.on('message', msg => {
  if (msg.content.includes('http://leakthefuckinglinkonemoretimeidareyou.eu-4.evennode.com')) {
    msg.reply('Please dont expose the link or else raids will happen');
+   
+  client.on('message', msg => {
+ if (msg.content.includes('sign up')) {
+  msg.reply('when i learn php yes or get another developer');
+   client.on('message', msg => {
+ if (msg.content.includes('patched')) {
+  msg.reply('Wrapper Online isnt patched.');
 
 
    
    
   
-  } else if (msg.content === 'importingg' ) {
-    msg.reply('never')
-} else if (msg.content === 'sign up' ) {
-    msg.reply('when i learn php')
+
  } else if (msg.content === 'hi' ) {
     msg.reply('hi how are u doing')
-  } else if (msg.content === 'patched' ) {
-     msg.reply('Wrapper Online isnt patched.')
+  
    } else if (msg.content === 'your grounded' ) {
       msg.reply('no u')
     } else if (msg.content === 'i hate raiders' ) {
