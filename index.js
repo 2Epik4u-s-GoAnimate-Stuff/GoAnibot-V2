76bf45aa3f002093333d82fdc5f07fd6a89b2a4d
2Epik4u <justinabossmlg@gmail.com>
2020-07-13 23:11:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,21 +11,13 @@ client.on("ready", () =>{
         }
     });
  });
-
-     
-     client.on('msg', message => {
-   if (message.content.startsWith("wrappersay ")) {
-      message.delete(1000); //Supposed to delete message
-      message.channel.send(message.content.slice(5, message.content.length));
-   }
-});
-   
-
-
-   
- else if ('msg', msg => {
+client.on('message', msg => {
  if (msg.content.includes('importing')) {
   msg.reply('never lol just edit it in dummy');
+   
+
+} else if (msg.content.includes('<@628607192806981632>')) {
+  msg.reply('Did you just fricken ping me. Thats it! you are grounded grounded grounded FOREVER.');
   
   } else if (msg.content.includes('Can 2Epik4u be grounded')) {
   msg.reply('no because hes my dad and hes god :rage:');
