c1b5d229f23c995dc595885f5e2b174615f2be67
2Epik4u <justinabossmlg@gmail.com>
2020-07-18 12:49:17 -0400

    client

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on("ready", () =>{
 });
 
 //discord help command
-bot.on("message", message => {
+client.on("message", message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
