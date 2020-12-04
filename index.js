6d33df0a14db8cee509a4a2ab93d66019fc19357
2Epik4u <justinabossmlg@gmail.com>
2020-12-04 13:04:36 -0400

    Removed

--- index.js
+++ index.js
@@ -224,93 +224,12 @@ client.on('message', message => {
 
 //ground cmd
 //ground cmd
-client.on('message', message => {
-	// Ignore messages that aren't from a guild
-	if (!message.guild) return;
-  
-	// If the message content starts with "!kick"
-	if (message.content.startsWith('|ground')) {
-	  // Assuming we mention someone in the message, this will return the user
-	  // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-	  const user = message.mentions.users.first();
-	  // If we have a user mentioned
-	  if (user) {
-		// Now we get the member from the user
-		const member = message.guild.member(user);
-		// If the member is in the guild
-		if (member) {
-		  /**
-		   * Kick the member
-		   * Make sure you run this on a member, not a user!
-		   * There are big differences between a user and a member
-		   */
-		  message.channel.send(`${user} is gunsis they wil go 2 dare room  `).then(() => {
-			// We let the message author know we were able to kick the person
-			
-		  }).catch(err => {
-			// An error happened
-			// This is generally due to the bot not being able to kick the member,
-			// either due to missing permissions or role hierarchy
-			message.reply('I was unable to ground the member');
-			// Log the error
-			console.error(err);
-		  });
-		} else {
-		  // The mentioned user isn't in this guild
-		  message.reply('That user isn\'t in this guild!');
-		}
-	  // Otherwise, if no user was mentioned
-	  } else {
-		message.reply('who ar u groundin???');
-	  }
-	}
-  });
+
   
 
 
 
-//unground command
-  client.on('message', message => {
-	// Ignore messages that aren't from a guild
-	if (!message.guild) return;
-  
-	
-	if (message.content.startsWith('|unground')) {
-	  // Assuming we mention someone in the message, this will return the user
-	  // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-	  const user = message.mentions.users.first();
-	  // If we have a user mentioned
-	  if (user) {
-		// Now we get the member from the user
-		const member = message.guild.member(user);
-		// If the member is in the guild
-		if (member) {
-		  /**
-		   * Kick the member
-		   * Make sure you run this on a member, not a user!
-		   * There are big differences between a user and a member
-		   */
-		 message.channel.send(`${user} is ungunsis  day can go 2 chuk e cheez  `).then(() => {
-			// We let the message author know we were able to kick the person
-			
-		  }).catch(err => {
-			// An error happened
-			// This is generally due to the bot not being able to kick the member,
-			// either due to missing permissions or role hierarchy
-			message.reply('srry but i cant ground him he has som hax or somethin');
-			// Log the error
-			console.error(err);
-		  });
-		} else {
-		  // The mentioned user isn't in this guild
-		  message.reply('That user isn\'t in this guild!');
-		}
-	  // Otherwise, if no user was mentioned
-	  } else {
-		message.reply('who ar u ungrunsis');
-	  }
-	}
-  });
+
 
 
 
@@ -394,6 +313,12 @@ client.on('message', msg => {
 
 	if (msg.startsWith === '|shutup' ) {
 		msg.reply('no')
+	}
+	if (msg.startsWith === '|ground' ) {
+		msg.reply('That command has been deleted.')
+	}
+	if (msg.startsWith === '|unground' ) {
+		msg.reply('That command has been deleted.')
 	}
 	if (msg.content === 'mj:online' ) {
 		msg.reply('yes, but epik denies it...')
