fb4977fb42fc44a6b0d1efe7c9fb0f2479611018
2Epik4u <justinabossmlg@gmail.com>
2021-03-08 10:32:53 -0500

    Update goanibot.js

--- goanibot.js
+++ goanibot.js
@@ -304,7 +304,7 @@ client.on('message', message => {
 
     if (message.mentions.has(client.user.id)) {
         message.channel.send('prefix is "ga " with a space.');
-    };
+ 
 });
 	 
 // make bot say a thing
