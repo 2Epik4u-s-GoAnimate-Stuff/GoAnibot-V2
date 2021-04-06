b342a1471902a3ff81dba1630bbcda982f10601d
Thunderzapper <justinabossmlg@gmail.com>
2021-04-05 22:10:13 -0400

    commands

---
+++ commands/ground.js
@@ -0,0 +1,20 @@
+﻿require('discord.js')
+
+module.exports = {
+    slash: 'both',
+    testOnly: true,
+    description: 'Ground your enemies (or friends)',
+    minArgs: 2,
+    expectedArgs: '<who> <for> <time>',
+    callback: ({ message, args }) => {
+        const [who, reason, time] = args
+
+
+
+        if (message) {
+            message.reply('')
+        }
+
+        return message
+    },
+}
